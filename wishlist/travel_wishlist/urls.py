1270453037da493303faa6d5bc638319770caed2
Estelle <epfiles@blah.com>
2026-03-19 00:32:22 -0500

    Django Wishlist: video 5

--- wishlist/travel_wishlist/urls.py
+++ wishlist/travel_wishlist/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     # state url path for homepage ('')
     path('', views.place_list, name='place_list'),
+    # state url path for visited request
+    path('visited', views.places_visited, name='places_visited'),
     # state url path for about page
     path('about', views.about, name='about')
 ]
--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -23,13 +23,20 @@ def place_list(request):
     places = Place.objects.filter(visited=False).order_by('name')
     # create new_place_form to create HTML
     new_place_form = NewPlaceForm()
-    # return render request
+    # return render request with data
     return render(request, 'travel_wishlist/wishlist.html', {'places': places, 'new_place_form': new_place_form})
 
+# create function to handle visited page url request
+def places_visited(request):
+    # get list of visited places
+    visited = Place.objects.filter(visited=True)
+    # return render request with data
+    return render(request, 'travel_wishlist/visited.html', { 'visited': visited })
 # create function to handle about page url request
+
 def about(request):
     # create data to use as part of response
     author = 'Estelle'
     about = 'A website to create a list a list of places to visit'
-    # return render request
+    # return render request with data
     return render(request, 'travel_wishlist/about.html', {'author': author, 'about': about})
